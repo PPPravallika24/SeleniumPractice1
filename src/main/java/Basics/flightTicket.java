190e9279592484360dea48bdde465655fdb95a47
PPPravallika24 <ppdpravallika@gmail.com>
2022-03-28 21:50:46 -0500

    adding Adults and checking destination

--- src/main/java/Basics/flightTicket.java
+++ src/main/java/Basics/flightTicket.java
@@ -4,28 +4,42 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 
 public class flightTicket {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver-2");
         WebDriver driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-        List<WebElement> Options=driver.findElements(By.xpath("//input[@name='ctl00$mainContent$rbtnl_Trip']"));
-        System.out.println(Options);
+        driver.manage().window().maximize();
+        List<WebElement> Options=driver.findElements(By.cssSelector("#travelOptions"));
         for(int i=0;i<Options.size();i++){
-            if(Options.contains("OneWay")){
+            String radio=Options.get(i).getText();
+            System.out.println(radio);
+            if(radio.contains("One Way")){
                 driver.findElement(By.xpath("//input[@id='ctl00_mainContent_ddl_originStation1_CTXT']")).click();
                 driver.findElement(By.xpath("//a[@text='Vijayawada (VGA)']")).click();
                 driver.findElement(By.xpath("//input[@id='ctl00_mainContent_ddl_destinationStation1_CTXT']")).click();
                 driver.findElement(By.xpath("(//a[@text='Jabalpur (JLR)'])[2]")).click();
-
-
+                driver.findElement(By.id("divpaxinfo")).click();
+                int j=1;
+                while(j<4){
+                    driver.findElement(By.id("hrefIncAdt")).click();
+                }
+                Select dropdown=new Select(driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency")));
+                dropdown.selectByValue("AED");
+            }
+            else{
 
             }
         }
-        //driver.close();
+        driver.close();
 
 
     }
