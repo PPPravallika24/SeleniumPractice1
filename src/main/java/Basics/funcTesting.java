cf4fa39c88f1b944b8b43ab324e933dfcada1df3
PPPravallika24 <ppdpravallika@gmail.com>
2022-03-31 15:47:08 -0500

    expicit wait added for some functionality

--- src/main/java/Basics/funcTesting.java
+++ src/main/java/Basics/funcTesting.java
@@ -4,7 +4,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
@@ -13,9 +18,32 @@ public class funcTesting {
         System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver-2");
         WebDriver driver=new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/seleniumPractise/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         String[] name={"Cauliflower","Cucumber","Beetroot"};
         Thread.sleep(3000);
+        addItems(driver,name);
 
+        driver.findElement(By.xpath("//a[@class='cart-icon']")).click();
+        driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+
+        driver.findElement(By.className("promoCode")).sendKeys("rahulshettyacademy");
+        driver.findElement(By.className("promoBtn")).click();
+        //explicit wait
+        WebDriverWait w =new WebDriverWait(driver,5);
+        w.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.promoInfo")));
+        System.out.println(driver.findElement(By.cssSelector("span.promoInfo")).getText());
+        driver.findElement(By.xpath("//button[text()='Place Order']")).click();
+        Thread.sleep(3000);
+
+        Select dropdown=new Select(driver.findElement(By.xpath("//option[text()='Select']")));
+        dropdown.selectByValue("United States");
+        driver.findElement(By.className("chkAgree")).click();
+        driver.findElement(By.xpath("//button[text()='Proceed']")).click();
+
+    }
+
+    public static void addItems(WebDriver driver, String[] name){
         int j=0;
         List<WebElement> products=driver.findElements(By.xpath("//h4[@class='product-name']"));
         for(int i=0;i<products.size();i++){
@@ -31,6 +59,7 @@ public class funcTesting {
                 break;
             }
         }
+
 
 
     }
